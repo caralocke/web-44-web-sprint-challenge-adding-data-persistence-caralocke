16b01962fc72324c505736fe2dc8f8d79f674d69
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-13 16:24:49 -0400

    Update router.js
    Fixed typo trying to get projects endpoint to work.

--- api/project/router.js
+++ api/project/router.js
@@ -3,7 +3,7 @@ const Projects = require('./model')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    await Project.getProjects()
+    await Projects.getProjects()
         .then(projects => {
             res.status(200).json(projects)
         })
