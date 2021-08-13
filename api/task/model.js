d70f871b7ec4751cad488d82c4b4fe094d6c39d5
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-13 17:23:45 -0400

    Tasks endpoint
    Attempting to set up the endpoints needed for tasks.

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,25 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig')
+
+function findAll() {
+    return db('tasks')
+    .select('tasks.*', 'projects.project_name', 'projects.project_description', 'projects.project_id')
+    .join('projects', 'tasks.project_id', 'projects.project_id')
+  ;
+}
+
+async function findById(task_id) {
+    return await db('tasks')
+    .where('task_id', task_id)
+    .first();
+}
+
+async function add(task) {
+    const [task_id] = await db('tasks').insert(task)
+    return task_id
+}
+
+module.exports = {
+    findAll,
+    findById,
+    add
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,27 @@
-// build your `/api/tasks` router here
+const express = require('express')
+const Tasks = require('./model')
+const router = express.Router()
+  
+
+router.get('/', (req, res, next) => {
+     Tasks.findAll()
+        .then(tasks => {
+            res.status(200).json(tasks)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const newTask = req.body
+    Tasks.add(newTask)
+        .then((task_id) => {
+            Tasks.findById(task_id)
+                .then(task => {
+                    res.status(201).json(task)
+                })
+                .catch(next)
+        })
+        .catch(next)
+})
+
+module.exports = router
