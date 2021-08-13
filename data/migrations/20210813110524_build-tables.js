2a2992319eac99486c924a9959ff56d14f9fe799
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-13 07:31:17 -0400

    Completed code needed to create the project_resources table.

--- data/migrations/20210813110524_build-tables.js
+++ data/migrations/20210813110524_build-tables.js
@@ -12,10 +12,42 @@ exports.up = function(knex) {
       table.string('resource_name').notNullable().unique()
       table.string('resource_description', 256)
   })
+  .createTable('tasks', (table) => {
+      table.increments('task_id')
+      table.string('task_description', 256).notNullable()
+      table.string('task_notes')
+      table.boolean('task_completed').defaultTo(false)
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+  })
+  .createTable('project_resources', (table) => {
+      table.increments('project_resources_id')
+      table.integer('project_id')   
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+      table.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resorce_id')
+        .inTable('resources')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+  })
 };
 
 exports.down = function(knex) {
   return knex.schema
+  .dropTableIfExists('project_resources')
+  .dropTableIfExists('tasks')
   .dropTableIfExists('resources')
   .dropTableIfExists('projects')
 };
